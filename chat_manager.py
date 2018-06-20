c624ce5668c7db48ed5d6d57f7d4b31a917593ed
yegorf1 <yegorf1@gmail.com>
2018-06-20 07:58:13 +0300

    Initial commit

---
+++ chat_manager.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+database = client['vk_notifications']
+
+
+def add_admin(userid):
+    database.admins.insert_one({'userid': userid})
+
+
+def get_admins():
+    database.admins.distinct('userid')
+
+
+def is_subscribed(subscription_type: str, peer: str, id: str):
+    count = database.subscriptions.count({'peer': peer, 'type': subscription_type, subscription_type: id})
+    return count > 0
+
+
+def subscribe_group(peer_id, group_id):
+    database.subscriptions.insert_one({'peer': peer_id, 'type': 'group', 'group': group_id})
+
+
+def is_admin(chat_id, user_id):
+    return user_id in get_admins()
+
+
+def get_peers(subscription_type, id):
+    return database.subscriptions.distinct('peer', {'type': subscription_type, subscription_type: id})
+
+
+def get_subscriptions(peer_id):
+    return database.subscriptions.find({'peer': peer_id})
+
+
+def subscribe_user(peer_id, user_id, filter=None):
+    database.subscriptions.insert_one({'peer': peer_id, 'type': 'user', 'user': user_id, 'filter': filter})
---
+++ main.py
@@ -0,0 +1,207 @@
+import re
+import time
+import vk_api
+import random
+import argparse
+
+import chat_manager
+import message_parser
+
+from vk_api.longpoll import VkLongPoll, VkEventType, Event
+
+from threading import Thread
+
+pin_code = random.randint(10000, 999999)
+message_parser = message_parser.MessageParser(command_symbol=':')
+vk: vk_api.VkApi = None
+running = False
+parsed_args = {}
+
+
+def new_post(post):
+    sub_type = 'group' if post['source_id'] < 0 else 'user'
+    id = abs(post['source_id'])
+
+    for peer in chat_manager.get_peers(sub_type, id):
+        vk.messages.send(peer_id=peer, attachment='wall{0}_{1}'.format(post['source_id'], post['post_id']))
+
+
+def read_feed():
+    last_check = time.time()
+
+    while running:
+        items = vk.newsfeed.get(start_time=int(last_check))["items"]
+
+        for item in items:
+            new_post(item)
+
+        last_check = time.time()
+        time.sleep(parsed_args.wait)
+
+
+def pincode_command(event: Event, pin: str):
+    global pin_code
+
+    old_pin_code = pin_code
+    pin_code = random.randint(10000, 999999)
+    print('New pin-code: {0}'.format(pin_code))
+
+    if pin == str(old_pin_code):
+        response = vk.users.get(user_ids=event.user_id, fields=['first_name', 'last_name'])
+        username = '{first_name} {lastname}'.format(**response)
+
+        chat_manager.add_admin(event.user_id)
+
+        return '*{0} ({1}) was promoted.\nPin-code changed.'.format(event.user_id, username)
+    else:
+        return 'Pin-code mismatched.\nPin-code changed.'
+
+
+def list_subscriptions(event: Event, *args):
+    subscriptions = chat_manager.get_subscriptions(event.peer_id)
+
+    if subscriptions.count() == 0:
+        return 'You are not subscribed for anything.'
+
+    message = 'Your subscriptions:\n'
+
+    for subscription in subscriptions:
+        sid = subscription[subscription['type']]
+        if subscription['type'] == 'group':
+            group = vk.groups.getById(group_id=sid)[0]
+            name = group['name']
+            screen_name = group['screen_name']
+        elif subscription['type'] == 'user':
+            user = vk.users.get(user_ids=sid, fields='screen_name')[0]
+            name = user['first_name'] + ' ' + user['last_name']
+            screen_name = user['screen_name']
+        else:
+            continue
+
+        message += '{0} (https://vk.com/{1})\n\n'.format(name, screen_name)
+
+    return message
+
+
+def subscribe_group(event: Event, link: str, *args):
+    if event.from_chat and not chat_manager.is_admin(event.chat_id, event.user_id):
+        return 'You are not admin.'
+
+    try:
+        group_id = link.split('com/')[1]
+        resp = vk.groups.getById(group_id=group_id)
+
+        if 'error' in resp:
+            return 'Could not find group with id {0}'.format(group_id)
+        group_name = resp[0]['name']
+
+        if chat_manager.is_subscribed('group', event.peer_id, resp[0]["id"]):
+            return 'Already subscribed for {0}'.format(group_name)
+
+        chat_manager.subscribe_group(event.peer_id, resp[0]["id"])
+
+        return 'Subscribed for {0}'.format(group_name)
+    except:
+        return 'Could not subscribe'
+
+
+def subscribe_user(event: Event, link: str, filter: str= '', *args):
+    if event.from_chat and not chat_manager.is_admin(event.chat_id, event.user_id):
+        return 'You are not admin.'
+
+    try:
+        user_id = link.split('com/')[1]
+        resp = vk.users.get(user_ids=user_id)
+
+        if 'error' in resp:
+            return 'Could not find user with id {0}'.format(user_id)
+        user_name = '{first_name} {last_name}'.format(**resp[0])
+
+        if chat_manager.is_subscribed('user', event.peer_id, resp[0]["id"]):
+            return 'Already subscribed for {0}'.format(user_name)
+
+        chat_manager.subscribe_user(event.peer_id, resp[0]["id"], filter)
+
+        return 'Subscribed for {0}'.format(user_name)
+    except Exception as e:
+        return 'Could not subscribe'
+
+
+def new_message(event: Event):
+    if event.from_me:
+        return
+
+    if event.from_group:
+        return
+
+    answer = None
+    try:
+        res = message_parser.parse(event.text)
+        if res is not None:
+            action, args = res
+            answer = action(event, *args)
+    except ValueError as e:
+        answer = 'Error at parsing: {0}'.format(e)
+    except Exception as e:
+        answer = str(e)
+
+    if answer is None:
+        return
+
+    vk.messages.send(peer_id=event.peer_id, message=answer)
+
+
+def main(login, password):
+    print('Admin pin code: {0}'.format(pin_code))
+
+    vk_session = vk_api.VkApi(login, password)
+
+    vk_session.auth()
+    global vk
+    vk = vk_session.get_api()
+
+    longpoll = VkLongPoll(vk_session)
+
+    global running
+    try:
+        running = True
+
+        news_thread = Thread(target=read_feed)
+        news_thread.start()
+
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                new_message(event)
+    except InterruptedError:
+        running = False
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser(description='Re-sends all information from ITMO teachers to student groups.')
+    args_parser.add_argument('--login', '-l', type=str, dest='login', required=True, help='VK login')
+    args_parser.add_argument('--password', '-p', type=str, dest='password', required=True, help='VK password')
+    args_parser.add_argument('--wait', '-w', type=float, dest='wait', help='Time to wait between news checks',
+                             default=3)
+
+    parsed_args = args_parser.parse_args()
+
+    message_parser.add_command('pincode',
+                               action=pincode_command,
+                               help_message='Activates admin privileges by pincode',
+                               args_description='pincode')
+
+    message_parser.add_command('list',
+                               action=list_subscriptions,
+                               help_message='Lists all subscriptions')
+
+    message_parser.add_command('subscribe_group',
+                               action=subscribe_group,
+                               help_message='Subscribe group for current chat for notifications',
+                               args_description='link')
+
+    message_parser.add_command('subscribe_user',
+                               action=subscribe_user,
+                               help_message='Connect user posts to current chat',
+                               args_description='link [filter]')
+
+    main(parsed_args.login, parsed_args.password)
